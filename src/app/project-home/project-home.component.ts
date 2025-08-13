3e53f2e228d0e26dd14583d511d4b9363628e566
Gavin Crubaugh <Gavincru2001@gmail.com>
2025-08-13 13:16:53 -0500

    Created a Nav

---
+++ src/app/project-home/project-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-home.component.html',
+  styleUrl: './project-home.component.css'
+})
+export class ProjectHomeComponent {
+
+}
